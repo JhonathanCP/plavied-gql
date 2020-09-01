7a99b077e3bdb00dfbe1e3f9ee58c89d49d5b9e3
Your Name <you@example.com>
2020-09-01 10:16:31 -0500

    script updated

--- script.sql
+++ script.sql
@@ -1,29 +1,135 @@
-CREATE TABLE users (
+CREATE TABLE administrador(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR,
+  lastname VARCHAR,
+  dni INTEGER unique,
+  email VARCHAR UNIQUE NOT null,
+  celular VARCHAR,
+  gender VARCHAR,
+  foto VARCHAR,
+  fecha_nacimiento VARCHAR,
+  calle VARCHAR,
+  ciudad VARCHAR,
+  provincia VARCHAR,
+  distrito VARCHAR,
+  password VARCHAR
+);
+
+create table profesor (
+	id serial PRIMARY KEY NOT NULL,
+    name VARCHAR,
+    lastname VARCHAR,
+    dni INTEGER unique,
+    email VARCHAR UNIQUE,
+    celular VARCHAR,
+    gender VARCHAR,
+    foto VARCHAR,
+    fecha_nacimiento VARCHAR,
+	calle VARCHAR,
+	ciudad VARCHAR,
+	provincia VARCHAR,
+	distrito VARCHAR,
+    cv VARCHAR,
+    isAccepted BOOLEAN,
+    password VARCHAR
+);
+
+create table tutor (
+	id serial PRIMARY KEY NOT NULL,
+    name VARCHAR,
+    lastname VARCHAR,
+    dni INTEGER unique,
+    email VARCHAR UNIQUE,
+    celular VARCHAR,
+    gender VARCHAR,
+    foto VARCHAR,
+    fecha_nacimiento VARCHAR,
+	calle VARCHAR,
+	ciudad VARCHAR,
+	provincia VARCHAR,
+	distrito VARCHAR,
+    cv VARCHAR,
+    isAccepted BOOLEAN,
+    password VARCHAR
+);
+
+CREATE TABLE apoderado (
     id serial PRIMARY KEY NOT NULL,
     name VARCHAR,
     lastname VARCHAR,
-    email VARCHAR UNIQUE NOT NULL
+    dni INTEGER unique,
+    email VARCHAR UNIQUE,
+    celular VARCHAR,
+    gender VARCHAR,
+    foto VARCHAR,
+    fecha_nacimiento VARCHAR,
+	calle VARCHAR,
+	ciudad VARCHAR,
+	provincia VARCHAR,
+	distrito VARCHAR,
+	password VARCHAR
 );
 
-CREATE TABLE authors (
+create table colegio (
+	id serial primary key not null,
+	nombre VARCHAR,
+	calle VARCHAR,
+	ciudad VARCHAR,
+	provincia VARCHAR,
+	distrito VARCHAR,
+	codigo_ugel VARCHAR unique
+);
+
+CREATE TABLE alumno (
     id serial PRIMARY KEY NOT NULL,
     name VARCHAR,
-    country VARCHAR,
-    register_by INTEGER,
-    FOREIGN KEY (register_by) REFERENCES users (id)
+    lastname VARCHAR,
+    dni INTEGER unique,
+    email VARCHAR UNIQUE,
+    celular VARCHAR,
+    gender VARCHAR,
+    foto VARCHAR,
+    fecha_nacimiento VARCHAR,
+	calle VARCHAR,
+	ciudad VARCHAR,
+	provincia VARCHAR,
+	distrito VARCHAR,
+    grado INTEGER,
+    studies_in INTEGER,
+    parent INTEGER,
+    tutored_by INTEGER,
+    password VARCHAR,
+    foreign key (studies_in) references colegio (id),
+    foreign key (parent) references apoderado (id),
+    foreign key (tutored_by) references tutor (id)
 );
 
-CREATE TABLE books (
-    id serial PRIMARY KEY NOT NULL,
-    title VARCHAR,
-    description VARCHAR,
-    quantity INTEGER,
-    price INTEGER,
-    writted_by INTEGER,
-    register_by INTEGER,
-    FOREIGN KEY (writted_by) REFERENCES authors (id),
-    FOREIGN KEY (register_by) REFERENCES users (id)
+create table curso (
+	id serial primary key not null,
+	nombre VARCHAR,
+	descripcion VARCHAR,
+	horas_totales INTEGER,
+	grado INTEGER,
+	contenido VARCHAR,
+	teached_by INTEGER,
+	foreign key (teached_by) references profesor (id)
 );
 
+create table reforzamiento (
+	id serial PRIMARY KEY NOT NULL,
+	categoria VARCHAR,
+	descripcion VARCHAR,
+	contenido VARCHAR,
+	teached_in INTEGER,
+	foreign key (teached_in) references curso (id)
+);
 
-ALTER TABLE users ADD COLUMN password VARCHAR;
+create table matricula (
+	id serial primary key not null,
+	signed INTEGER,
+	signed_in INTEGER,
+	progreso INTEGER,
+	notas INTEGER[],
+	foreign key (signed) references alumno (id),
+	foreign key (signed_in) references curso (id)
+);
